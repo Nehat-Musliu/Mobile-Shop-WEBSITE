1b566581adff2616809e4a0cb596a5bb83dab70f
Nehat-Musliu <nm70221@ubt-uni.net>
2025-02-02 22:06:48 +0100

    Create seeders.sql

---
+++ seeders.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (name,surname,email, password_hash,role) VALUES
+    ('Nehat Musliu','nehat@gmail.com', '$2y$10$v7ALdBDH.fhh6Ta62RTxWOvKjNTJi3jXtuRLzhc7bko/3WMegoS4i','admin'),  -- 12345678
+    ('Valon Zuka','valonzuka@gmail.com', '$2y$10$v7ALdBDH.fhh6Ta62RTxWOvKjNTJi3jXtuRLzhc7bko/3WMegoS4i','user'),  -- 12345678
+    ('Senad ','senad@gmail.com', '$2y$10$v7ALdBDH.fhh6Ta62RTxWOvKjNTJi3jXtuRLzhc7bko/3WMegoS4i','user');  -- 12345678
